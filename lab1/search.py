85c826abe5cc07b4c2a95cd408ccd900339eb789
Ardita Salja <ardita.salja@gmail.com>
2019-05-10 19:02:37 +0200

    lab1

---
+++ lab1/search.py
@@ -0,0 +1,328 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+import copy
+
+class SearchNode:
+    """
+    This class represents a node in the graph which represents the search problem.
+    The class is used as a basic wrapper for search methods - you may use it, however
+    you can solve the assignment without it.
+
+    REMINDER: You need to fill in the backtrack function in this class!
+    """
+
+    def __init__(self, position, parent=None, transition=None, cost=0, heuristic=0):
+        """
+        Basic constructor which copies the values. Remember, you can access all the 
+        values of a python object simply by referencing them - there is no need for 
+        a getter method. 
+        """
+        self.position = position
+        self.parent = parent
+        self.cost = cost
+        self.heuristic = heuristic
+        self.transition = transition
+
+    def isRootNode(self):
+        """
+        Check if the node has a parent.
+        returns True in case it does, False otherwise
+        """
+        return self.parent == None 
+
+    def unpack(self):
+        """
+        Return all relevant values for the current node.
+        Returns position, parent node, cost, heuristic value
+        """
+        return self.position, self.parent, self.cost, self.heuristic
+
+
+    def backtrack(self):
+        """
+        Reconstruct a path to the initial state from the current node.
+        Bear in mind that usually you will reconstruct the path from the 
+        final node to the initial.
+        """
+        moves = []
+        # make a deep copy to stop any referencing isues.
+        node = copy.deepcopy(self)
+
+        if node.isRootNode(): 
+            # The initial state is the final state
+            return moves        
+
+        "**YOUR CODE HERE**"
+
+
+        curr_node = self
+        moves.append(curr_node.transition)
+        curr_node = node.parent
+        moves += curr_node.backtrack()
+        return moves
+
+        util.raiseNotDefined()
+
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    "*** YOUR CODE HERE ***"
+    stack = util.Stack()
+    visited=set()
+
+    #problem.getStartState()
+    start_node = SearchNode(problem.getStartState())
+    stack.push(start_node)
+    visited.add(start_node.position)
+
+    while(not stack.isEmpty()):
+        curr_node_stack = stack.pop()
+
+        visited.add(curr_node_stack.position)
+
+        if problem.isGoalState(curr_node_stack.position):
+            list_direction = curr_node_stack.backtrack()
+            list_direction.reverse()
+            return list_direction
+
+        for x in problem.getSuccessors(curr_node_stack.position):
+            if not x[0] in visited:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.parent = curr_node_stack
+                stack.push(succ_node)
+                #visited.add(succ_node.position)
+
+            else:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.parent = curr_node_stack
+                if problem.isGoalState(succ_node.position):
+                    list_direction = succ_node.backtrack()
+                    list_direction.reverse()
+                    return list_direction
+                #visited.add(succ_node.position)
+    util.raiseNotDefined()
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    queue = util.Queue()
+    visited = set()
+
+    problem.getStartState()
+    start_node = SearchNode(problem.getStartState())
+    queue.push(start_node)
+    visited.add(start_node.position)
+
+    while (not queue.isEmpty()):
+        curr_node_stack = queue.pop()
+        visited.add(curr_node_stack.position)
+
+        if problem.isGoalState(curr_node_stack.position):
+            list_direction = curr_node_stack.backtrack()
+            list_direction.reverse()
+            return list_direction
+
+        for x in problem.getSuccessors(curr_node_stack.position):
+            if not x[0] in visited:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+
+                succ_node.parent = curr_node_stack
+
+                visited.add(succ_node.position)
+                queue.push(succ_node)
+
+            else:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.parent = curr_node_stack
+                if problem.isGoalState(succ_node.position):
+                    list_direction = succ_node.backtrack()
+                    list_direction.reverse()
+                    return list_direction
+                visited.add(succ_node.position)
+
+    util.raiseNotDefined()
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    p_queue = util.PriorityQueue()
+    visited = set()
+
+    problem.getStartState()
+    start_node = SearchNode(problem.getStartState())
+    p_queue.push(start_node, start_node.cost)
+    visited.add(start_node.position)
+
+    while (not p_queue.isEmpty()):
+        curr_node_stack = p_queue.pop()
+        visited.add(curr_node_stack.position)
+
+        if problem.isGoalState(curr_node_stack.position):
+            list_direction = curr_node_stack.backtrack()
+            list_direction.reverse()
+            return list_direction
+
+        for x in problem.getSuccessors(curr_node_stack.position):
+            if not x[0] in visited:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.cost = x[2]
+                succ_node.parent = curr_node_stack
+                succ_node.cost += curr_node_stack.cost
+                visited.add(succ_node.position)
+                p_queue.push(succ_node, succ_node.cost)
+
+            else:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.parent = curr_node_stack
+                if problem.isGoalState(succ_node.position):
+                    list_direction = succ_node.backtrack()
+                    list_direction.reverse()
+                    return list_direction
+                visited.add(succ_node.position)
+    util.raiseNotDefined()
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    p_queue = util.PriorityQueue()
+    visited = set()
+
+    problem.getStartState()
+    start_node = SearchNode(problem.getStartState())
+    p_queue.push(start_node, start_node.cost)
+    visited.add(start_node.position)
+
+    while (not p_queue.isEmpty()):
+        curr_node_stack = p_queue.pop()
+        visited.add(curr_node_stack.position)
+
+        if problem.isGoalState(curr_node_stack.position):
+            list_direction = curr_node_stack.backtrack()
+            list_direction.reverse()
+            return list_direction
+
+        for x in problem.getSuccessors(curr_node_stack.position):
+            if not x[0] in visited:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.cost = x[2]
+                succ_node.parent = curr_node_stack
+                succ_node.cost += curr_node_stack.cost
+                priority = succ_node.cost + heuristic(succ_node.position, problem)
+                visited.add(succ_node.position)
+                p_queue.push(succ_node, priority)
+
+            else:
+                succ_node = SearchNode(x[0])
+                succ_node.transition = x[1]
+                succ_node.parent = curr_node_stack
+                succ_node.cost += curr_node_stack.cost
+                if problem.isGoalState(succ_node.position):
+                    list_direction = succ_node.backtrack()
+                    list_direction.reverse()
+                    return list_direction
+                visited.add(succ_node.position)
+    util.raiseNotDefined()
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
